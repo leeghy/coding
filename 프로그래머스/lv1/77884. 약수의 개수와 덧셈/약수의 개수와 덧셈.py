8a37c9ce9ae280c1b28dd134070cbfe0defcbfe0
leeghy <115767150+leeghy@users.noreply.github.com>
2023-05-01 20:35:26 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.47 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -0,0 +1,16 @@
+def divisor(num):
+    answer = []
+    for i in range(1, num+1):
+        if num % i == 0:
+            answer.append(i)
+    return answer
+
+def solution(left, right):
+    answer = 0
+    
+    for i in range(left, right+1):
+        if len(divisor(i)) % 2 == 0:
+            answer += i
+        else:
+            answer -= i
+    return answer
