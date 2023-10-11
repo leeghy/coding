0ef01f17e4bfb2ce349c8fe000e7eaf39b7eca04
leeghy <115767150+leeghy@users.noreply.github.com>
2023-10-11 22:35:40 +0900

    [Bronze V] Title: A+B - 3, Time: 232 ms, Memory: 17988 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		int T = scanner.nextInt();
+
+		int arr[] = new int[T];
+
+		for (int i = 0; i < T; i++) {
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+
+			arr[i] = a + b;
+		}
+		
+		for (int i=0;i<T;i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
